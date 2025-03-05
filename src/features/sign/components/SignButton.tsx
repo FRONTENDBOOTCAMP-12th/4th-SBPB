880351b1b019eeb04dbb3e027baf262bf771d83a
llhyeon <0617mosses@gmail.com>
2025-03-05 10:25:48 +0900

    [Feat] Sign 버튼 컴포넌트 개발
    - SignUp, SignIn 페이지에서 사용되는 버튼 컴포넌트 개발
    - prettier 세미콜론 사용 여부 수정
    
    Resolved #6

---
+++ src/features/sign/components/SignButton.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+
+interface SignButtonProps {
+  label?: string;
+  color?: 'black' | 'white';
+  useImage?: boolean;
+  kindImage?: 'google' | 'kakao' | 'email';
+}
+
+const images = {
+  google: '/google-icon.svg',
+  kakao: '/kakao-icon.svg',
+  email: '/email-icon.svg',
+};
+
+function SignButton({
+  label = '시작하기',
+  color = 'black',
+  useImage = false,
+  kindImage = 'google',
+}: SignButtonProps) {
+  const signButton =
+    color === 'black' ? (
+      <button
+        type="button"
+        className="flex justify-center gap-2 w-[15.625rem] py-3 rounded-sm bg-content-primary text-white border border-white text-xs"
+      >
+        {useImage && (
+          <Image src={images[kindImage]} alt="google" width={18} height={18} />
+        )}
+
+        {label}
+      </button>
+    ) : (
+      <button
+        type="button"
+        className="w-[15.625rem] py-3 rounded-sm bg-white text-content-primary  text-xs"
+      >
+        {label}
+      </button>
+    );
+
+  return signButton;
+}
+
+export default SignButton;
