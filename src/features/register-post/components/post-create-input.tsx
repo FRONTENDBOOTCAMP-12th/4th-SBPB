c49739393295275863ddf04479a6dcd810627cc7
akman12914 <rnlduadns98@naver.com>
2025-03-06 13:38:07 +0900

    [Feat] 글쓰기 작성 input 컴포넌트 완성

---
+++ src/features/register-post/components/post-create-input.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { PostCreateInputProps } from '@/features/register-post/types/post-create-input-field-props'
+
+function PostCreateInput({
+  label,
+  value,
+  onChange,
+  type = 'text',
+  placeholder = '',
+  customClass = '',
+  textLimit = 20,
+  isVisible = '',
+}: PostCreateInputProps) {
+  const [charCount, setCharCount] = useState(value.length)
+
+  useEffect(() => {
+    setCharCount(value.length)
+  }, [value])
+
+  return (
+    <div>
+      <label className="hidden">{label}</label>
+      {type === 'textarea' ? (
+        <textarea
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          className="bg-white w-[100%] h-screen rounded-tl-xl rounded-tr-xl p-3 placeholder-gray-600 "
+          rows={4}
+        />
+      ) : (
+        <div className="relative border-solid border-b-[0.2px] border-gray-200 mx-3">
+          <input
+            type="text"
+            value={value}
+            onChange={onChange}
+            placeholder={placeholder}
+            className={`w-[90%] relative  item-center pb-1.5 ${customClass}`}
+          />
+
+          <div
+            className={`absolute right-0 bottom-0 text-gray-400 text-sm ${isVisible}`}
+          >
+            <span className="text-white">{charCount}</span>
+            {textLimit && `/${textLimit}`}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default PostCreateInput
