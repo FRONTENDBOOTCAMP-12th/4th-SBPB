7fa9eecd5b99dc45fe08fefc2a52ecf975ec6f7e
llhyeon <0617mosses@gmail.com>
2025-03-06 10:28:33 +0900

    [Refactor] 회원가입 페이지 리팩토링
    - motion-framer를 사용해 애니메이션 추가
    - 애니메이션 적용을 위해 layout 수정
    
    Resolves #56

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
-import './globals.css'
+import { AnimatePresence } from 'framer-motion';
+import './globals.css';
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <AnimatePresence>
+        <body>{children}</body>
+      </AnimatePresence>
     </html>
-  )
+  );
 }
--- src/app/signup/email/page.tsx
+++ src/app/signup/email/page.tsx
@@ -1,12 +1,21 @@
+'use client';
+
 import Logo from '@/components/logo';
 import SignUpForm from '@/features/sign/components/signup-form';
+import { motion } from 'framer-motion';
 
 function SignUpEmail() {
   return (
-    <div className="absolute inset-x-0 inset-y-16">
-      <Logo w={178} h={101} className="mb-20" />
-      <SignUpForm />
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: -20 }}
+      transition={{ duration: 1 }}
+    >
+      <div className="absolute inset-x-0 inset-y-16">
+        <Logo w={178} h={101} className="mb-20" />
+        <SignUpForm />
+      </div>
+    </motion.div>
   );
 }
 
