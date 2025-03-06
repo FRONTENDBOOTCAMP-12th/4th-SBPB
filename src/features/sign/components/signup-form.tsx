b0fadbd82b7447c4fa1f8402bf564fae762c82df
llhyeon <0617mosses@gmail.com>
2025-03-06 09:41:27 +0900

    [Refactor] SignUpForm 리팩토링
    - form에 사용된 SignInput 컴포넌트에 isLabelShow 속성 추가
    
    Resolves #55

--- src/features/sign/components/signup-form.tsx
+++ src/features/sign/components/signup-form.tsx
@@ -8,24 +8,28 @@ function SignUpForm() {
         name="id"
         label="아이디"
         placeholder="8문자 이상, 숫자영문 조합"
+        isLabelShow
       />
       <SignInput
         type="email"
         name="email"
         label="이메일"
         placeholder="인증 가능한 이메일 주소"
+        isLabelShow
       />
       <SignInput
         type="password"
         name="password"
         label="비밀번호"
         placeholder="8문자 이상, 특수문자 포함"
+        isLabelShow
       />
       <SignInput
         type="password"
         name="passwordConfirm"
         label="비밀번호 확인"
         placeholder="같은 비밀번호를 입력해주세요"
+        isLabelShow
       />
       <SignButton label="관심지역 선택" color="white" className="mt-10" />
     </form>
