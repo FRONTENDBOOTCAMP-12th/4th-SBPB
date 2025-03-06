23f7676e056017d8943daa5bc652d4fced203bde
llhyeon <0617mosses@gmail.com>
2025-03-06 09:49:50 +0900

    [Refactor] Image태그에 priority 속성 추가
    - 첫 화면에 렌더링 되는 이미지에 priority 속성 추가하여 성능 개선
    
    Resolves #55

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,8 +10,7 @@ interface LogoProps {
 function Logo({ className = '', w = 100, h = 100 }: LogoProps) {
   return (
     <figure className={tm('flex justify-center', className)}>
-      <Image src="/logo.svg" alt="로고" width={w} height={h} />
-      <figcaption className="sr-only">로고</figcaption>
+      <Image src="/logo.svg" alt="로고" width={w} height={h} priority />
     </figure>
   );
 }
