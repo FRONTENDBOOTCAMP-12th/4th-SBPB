43db09960fdc4e13315cf7669b4f0f33dc0c5a13
leeginger <jeneve90@naver.com>
2025-03-07 14:09:08 +0900

    [Test] postplace
    - 코드 수정중중
    
    Resolves #16

--- src/features/post-place/components/post-place.tsx
+++ src/features/post-place/components/post-place.tsx
@@ -2,60 +2,27 @@
 "use client";
 import { tm } from '@/utils/tw-merge';
 import { useEffect, useState } from 'react';
-import { addRecipe, getRecipes } from '@/features/post-place/lib/recipes';
-import type { Recipe, Recipes } from '@/features/post-place/types/types';
 
 
 function PostPlace() {
-  const [data, setData] = useState<null | Recipes>(null);
+  
+  const [cities, setCities] = useState<string[]>([])
 
-  useEffect(() => {
-    let ignore = false;
-
-    getRecipes({ startIndex: 1, limit: 3 }).then((data) => {
-      if (!ignore) {
-        setData(data);
-      }
-    });
-
-    return () => {
-      ignore = true;
-    };
-  }, []);
-
-  // 레시피 추가 요청
-  const handleAdd = async (formData: FormData) => {
-    //await delay();
-
-    // <form> 내부 데이터 가져오기
-    const newRecipeName = formData.get('recipe') as string;
-
-    // 서버에 데이터 추가 요청
-    const newRecipe = await addRecipe({
-      name: newRecipeName,
-    });
-
-    // 서버의 응답을 받아서, 클라이언트 앱 화면 업데이트 요청
-    if (data) {
-      const nextData: Recipes = {
-        ...data,
-        recipes: [newRecipe, ...data.recipes],
-      };
-
-      setData(nextData);
-    }
-  };
-
+  function handleSearch(formdata:FormData){
+    const place = formdata.get('place');
+    setCities([...cities,place])
+  }
     return (
       <>
       <h2 className={tm('text-xl', 'mb-[12px]')}>글에 추가할 장소를 검색해 보세요</h2>
-      <form action={handleAdd} className={tm('mb-[32px]')}>
-        <label className="sr-only">카드 검색</label>
+      <form action={handleSearch} className={tm('mb-[32px]')}>
+        <label className="sr-only">장소 검색</label>
         <div className={tm('flex','border-black', 'border-[1px]', 'h-[40px]','rounded-lg', 'px-[12px]')}>
-          <input type="text" name="recipe"  placeholder='원하는 지역을 검색해 보세요 ' className={tm('w-full')}/>
+          <input type="search" name="place"  placeholder='원하는 지역을 검색해 보세요 ' className={tm('w-full')}/>
           <button
             type="submit"
             title="검색"
+            aria-label='검색'
             className={tm(
               'cursor-pointer opacity-90'
             )}
@@ -67,25 +34,7 @@ function PostPlace() {
         </div>
       </form>
       <ul className="flex flex-col gap-2 my-2">
-        {data?.recipes?.map((item: Recipe) => (
-          <li
-            key={item.id}
-            className="flex items-center justify-between bg-[#0D0E0F] px-[12px] h-[46px] border-1 border-slate-400 rounded-lg"
-          >
-            <div className={tm('flex', 'gap-[7px]')}>
-              <svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0.75 6.44225C0.75 10.0813 3.933 13.0903 5.34225 14.2438C5.54475 14.4088 5.646 14.4928 5.79675 14.5348C5.91375 14.5678 6.08625 14.5678 6.20325 14.5348C6.354 14.4928 6.45525 14.4095 6.65775 14.2438C8.067 13.0903 11.25 10.0813 11.25 6.44225C11.2493 5.75903 11.1131 5.08273 10.8492 4.45254C10.5853 3.82235 10.1989 3.2508 9.7125 2.771C8.72333 1.79528 7.38942 1.24878 6 1.25C4.61058 1.24878 3.27667 1.79528 2.2875 2.771C1.80106 3.25077 1.41468 3.82232 1.15076 4.45251C0.886835 5.08271 0.750617 5.75902 0.75 6.44225Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M4.5 5.75C4.5 6.14782 4.65804 6.52936 4.93934 6.81066C5.22064 7.09196 5.60218 7.25 6 7.25C6.39782 7.25 6.77936 7.09196 7.06066 6.81066C7.34196 6.52936 7.5 6.14782 7.5 5.75C7.5 5.35218 7.34196 4.97064 7.06066 4.68934C6.77936 4.40804 6.39782 4.25 6 4.25C5.60218 4.25 5.22064 4.40804 4.93934 4.68934C4.65804 4.97064 4.5 5.35218 4.5 5.75Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
-              <span className={tm('text-xs', 'text-white')}>{item.name}</span>
-            </div>
-            <button type="submit" title="장소 삭제">
-              <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M10.5 10.5L7 7M7 7L3.5 3.5M7 7L10.5 3.5M7 7L3.5 10.5" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
-            </button>
-          </li>
-        ))}
+        {cities.map(city => <li>{city}</li>)}
       </ul>
       </>
     );
