56d5fe24e54382d5f7d42560d6a2e922d81c2a5d
neulhi <neu1hi.work@gmail.com>
2025-03-07 09:29:11 +0900

    [Feat] Feed 페이지 게시물 컴포넌트 개발
    - UI 작업
    - 파비콘 추가
    
    Resolves #13

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,17 +1,47 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { tm } from '@/utils/tw-merge';
 import NavItems from '@/components/nav-items';
 import Profile from '@/components/profile';
+import FeedCard, {
+  getDummyFeedData,
+} from '@/features/feed/components/feed-card';
 import TagBar from '@/features/feed/components/tag-bar';
-import { tm } from '@/utils/tw-merge';
 
 function FeedPage() {
+  const dummyPosts = getDummyFeedData();
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  useEffect(() => {
+    if (isExpanded) {
+      setTimeout(() => {
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      }, 100);
+    }
+  }, [isExpanded]);
+
   return (
-    <div
-      className={tm(
-        'w-80 h-dvh m-auto flex flex-col items-center bg-[#efefef]'
-      )}
-    >
+    <div className={tm('w-80 m-auto flex flex-col items-center pb-24')}>
       <Profile />
       <TagBar />
+      {dummyPosts.map(({ id, user, post }, index) => (
+        <FeedCard
+          key={id}
+          id={id}
+          user={user}
+          post={post}
+          onExpand={() => {
+            if (index === dummyPosts.length - 1) {
+              setIsExpanded(true);
+            }
+          }}
+          onCollapse={() => setIsExpanded(false)}
+        />
+      ))}
       <NavItems />
     </div>
   );
