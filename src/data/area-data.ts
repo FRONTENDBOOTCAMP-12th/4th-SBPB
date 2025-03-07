8e775b0892bb9e4d00c0a91804878441f92c2e83
llhyeon <0617mosses@gmail.com>
2025-03-07 09:23:01 +0900

    [Feat] 타입 및 데이터 분리
    - 관심 지역 페이지 만드는 데에 필요한 데이터와 타입 분리
    
    Resolves #61

---
+++ src/data/area-data.ts
@@ -0,0 +1,87 @@
+export const areaData = [
+  {
+    name: '서울',
+    enName: 'seoul',
+    isSelected: false,
+  },
+  {
+    name: '부산',
+    enName: 'busan',
+    isSelected: false,
+  },
+  {
+    name: '충청북도',
+    enName: 'chungcheongbukdo',
+    isSelected: false,
+  },
+  {
+    name: '충청남도',
+    enName: 'chungcheongnamdo',
+    isSelected: false,
+  },
+  {
+    name: '대구',
+    enName: 'daegu',
+    isSelected: false,
+  },
+  {
+    name: '대전',
+    enName: 'daejeon',
+    isSelected: false,
+  },
+  {
+    name: '강원도',
+    enName: 'gangwondo',
+    isSelected: false,
+  },
+  {
+    name: '광주',
+    enName: 'gwangju',
+    isSelected: false,
+  },
+  {
+    name: '경기도',
+    enName: 'gyeonggido',
+    isSelected: false,
+  },
+  {
+    name: '경상북도',
+    enName: 'gyeongsangbukdo',
+    isSelected: false,
+  },
+  {
+    name: '경상남도',
+    enName: 'gyeongsangnamdo',
+    isSelected: false,
+  },
+  {
+    name: '인천',
+    enName: 'incheon',
+    isSelected: false,
+  },
+  {
+    name: '제주도',
+    enName: 'jeju',
+    isSelected: false,
+  },
+  {
+    name: '전라북도',
+    enName: 'jeollabukdo',
+    isSelected: false,
+  },
+  {
+    name: '전라남도',
+    enName: 'jeollanamdo',
+    isSelected: false,
+  },
+  {
+    name: '세종시',
+    enName: 'sejong',
+    isSelected: false,
+  },
+  {
+    name: '울산',
+    enName: 'ulsan',
+    isSelected: false,
+  },
+];
