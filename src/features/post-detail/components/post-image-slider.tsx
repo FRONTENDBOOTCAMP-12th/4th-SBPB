0a4a0a7fcf32596a48d74b709c8928c2a49267df
akman12914 <rnlduadns98@naver.com>
2025-03-07 15:39:12 +0900

    [Comment] 주석 수정

--- src/features/post-detail/components/post-image-slider.tsx
+++ src/features/post-detail/components/post-image-slider.tsx
@@ -34,19 +34,17 @@ function PostImageSlider({ images }: PostImageSliderProps) {
 
   return (
     <div className="relative w-full h-[232px] overflow-hidden rounded-t-2xl">
-      {/* 애니메이션을 적용할 슬라이더 컴포넌트 */}
       <AnimatePresence initial={false} custom={direction}>
-        {/* 슬라이더에서 각 이미지는 motion.div로 감싸고, key로 page를 설정해 주면 page가 변경될 때 애니메이션이 적용됩니다. */}
         <motion.div
-          key={page} // page가 변경될 때마다 새로운 key가 들어가므로 애니메이션을 재실행합니다.
-          custom={direction} // 방향을 custom prop으로 전달하여, 애니메이션에서 사용할 수 있도록 합니다.
-          variants={variants} // variants 객체를 사용하여 enter, center, exit 상태를 정의
-          initial="enter" // 처음 애니메이션 상태는 enter로 설정 (슬라이드가 들어올 때)
-          animate="center" // 애니메이션이 진행되면 center로 설정 (현재 페이지가 화면 중앙에 오게)
-          exit="exit" // exit 상태에서 나갈 때 애니메이션 설정 (슬라이드가 나갈 때)
+          key={page}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
           transition={{
-            x: { type: 'spring', stiffness: 300, damping: 50 }, // x 이동 애니메이션에 대한 세부 설정 (스프링 애니메이션)
-            opacity: { duration: 0.2 }, // 투명도 변화에 대한 설정 (0.2초 동안 opacity를 변경)
+            x: { type: 'spring', stiffness: 300, damping: 50 },
+            opacity: { duration: 0.2 },
           }}
           className="absolute w-full h-full" // 이미지가 전체 슬라이더 크기에 맞게 커버되도록 설정
         >
@@ -59,24 +57,23 @@ function PostImageSlider({ images }: PostImageSliderProps) {
         </motion.div>
       </AnimatePresence>
 
-      {/* 왼쪽 화살표 버튼 (슬라이드 왼쪽으로 이동) */}
       <div
         className="absolute top-0 left-0 z-10 flex items-center justify-center w-[50%] h-[100%] cursor-pointer"
         onClick={() => paginate(-1)} // 왼쪽으로 슬라이드 이동
       ></div>
 
-      {/* 오른쪽 화살표 버튼 (슬라이드 오른쪽으로 이동) */}
       <div
         className="absolute top-0 right-0 z-10 flex items-center justify-center w-[50%] h-[100%] cursor-pointer"
         onClick={() => paginate(1)} // 오른쪽으로 슬라이드 이동
       ></div>
+
       <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
         {images.map((_, i) => (
           <div
             key={i}
-            className={`w-3 h-3 rounded-full ${imageIndex(page) === i ? 'bg-gray-200' : 'bg-white'}`}
+            className={`w-3 h-3 rounded-full ${imageIndex(page) === i ? 'bg-gray-200' : 'bg-white'}`} // 현재 위치 표시
           />
-        ))}
+        ))}{' '}
       </div>
     </div>
   );
