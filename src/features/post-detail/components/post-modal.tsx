7516de27f2c085c710fc9c889d3048b9cc8afaca
akman12914 <rnlduadns98@naver.com>
2025-03-07 10:32:50 +0900

    [Feat] 수정/삭제 모달 포함 헤더

---
+++ src/features/post-detail/components/post-modal.tsx
@@ -0,0 +1,19 @@
+import { PostModalProps } from '../types/post-modal-types'
+
+function PostModal({ onEdit, onDelete }: PostModalProps) {
+  return (
+    <div className="bg-[url('/union.svg')] w-[66px] h-[100px] flex flex-col shadow-right">
+      <button
+        onClick={onEdit}
+        className="pt-8 py-2 mx-3 text-sm font-semibold border-solid border-b-2 "
+      >
+        수정
+      </button>
+      <button onClick={onDelete} className="py-2 mx-3 text-sm font-semibold ">
+        삭제
+      </button>
+    </div>
+  )
+}
+
+export default PostModal
