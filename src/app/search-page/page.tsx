139e6527a696a39bd18c383736cec8aeeb72e965
leeginger <jeneve90@naver.com>
2025-03-12 10:46:39 +0900

    [Feat] 게시글 검색바 구현
    - input에 focus
    
    Resolves #22

---
+++ src/app/search-page/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { tm } from '@/utils/tw-merge';
+import { useEffect, useRef } from "react";
+import SearchInput from '@/features/search-page/components/search-page';
+
+function SearchPage(){
+    const searchFormRef = useRef<{
+        focus: () => void;
+    }>(null);
+
+    useEffect(() => {
+      const clearId = setTimeout(() => {
+        const handles = searchFormRef.current;
+        if (handles) {
+          handles.focus();
+        }
+      }, 900);
+    
+      return () => {
+        clearTimeout(clearId);
+      };
+    });
+
+    return(
+        <>
+        <div className={tm('px-[14px]')}>
+            <SearchInput ref={searchFormRef}/>
+        </div>
+        </>
+    )
+}
+
+export default SearchPage;
