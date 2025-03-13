0ec468212a4bc694d1b467f20b894b4c0f01cec1
neulhi <neu1hi.work@gmail.com>
2025-03-13 17:25:22 +0900

    [Dev] 루트 layout.tsx에 메타데이터 설정 추가
    - 테스트로 app/feed/page.tsx 도 타이틀 추가
    
    Resolves #119

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,31 @@
 import './globals.css';
 import 'react-toastify/dist/ReactToastify.css';
-import Head from 'next/head';
 import Toast from '@/components/toast';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    default: '사방팔방 여행후기 공유 서비스 프로젝트', // 기본 페이지 제목
+    template: '%s | 사방팔방 여행후기 공유 서비스 프로젝트', // 동적 타이틀 적용
+  },
+  description:
+    '사방팔방은 방문했던 지역 명소나 맛집 등 다양한 장르의 국내 여행지를 다녀왔던 후기와 사진을 위치와 함께 공유하며 소통하는 여행 후기 공유 서비스 프로젝트입니다.', // 페이지 설명 (검색엔진 & SNS에서 표시)
+  openGraph: {
+    title: '사방팔방 여행후기 공유 서비스 프로젝트',
+    description:
+      '사방팔방은 방문했던 지역 명소나 맛집 등 다양한 장르의 국내 여행지를 다녀왔던 후기와 사진을 위치와 함께 공유하며 소통하는 여행 후기 공유 서비스 프로젝트입니다.',
+    url: 'https://sbpb4.netlify.app/', // 페이지 URL
+    siteName: '사방팔방',
+    images: [
+      {
+        url: '/open-graph.svg', // OG 이미지 URL
+        width: 1200,
+        height: 630,
+        alt: '사방팔방 여행 후기 공유 서비스 프로젝트 미리보기',
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
@@ -10,10 +34,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <Head>
-        <title>사방팔방</title>
-        <meta name="사방팔방" content="사방팔방 페이지입니다." />
-      </Head>
       <body>
         <Toast />
         {children}
