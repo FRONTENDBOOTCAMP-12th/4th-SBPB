a8c7663f6bdfc5f8cce0a1e1185514d7a4de9b36
llhyeon <0617mosses@gmail.com>
2025-03-18 15:19:48 +0900

    [Feat] 태그 입력 컴포넌트 개발
    - 태그 입력 시 사용되는 컴포넌트 개발
    
    Resolves #139

---
+++ src/features/register-post/components/tag-input.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { SetStateAction, useState } from 'react';
+
+interface TagInputProps {
+  label: string;
+  tags?: string[];
+  setTags: React.Dispatch<SetStateAction<string[]>>;
+}
+
+function TagInput({ label, tags, setTags }: TagInputProps) {
+  // 한글 조합 상태 관리
+  const [isComposing, setIsComposing] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  // 태그 등록 이벤트 핸들러
+  const handleTagEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (isComposing) return;
+    if (e.key !== 'Enter' && e.key !== ',') return;
+
+    const value = e.currentTarget.value.trim();
+
+    if (!value.length) return;
+
+    setTags((prev) => [...prev, value]);
+    setInputValue('');
+  };
+
+  return (
+    <div className="overflow-visible relative border-solid border-b-[0.2px]  border-gray-200 mx-3 pt-[20px] mb-[80px] flex flex-wrap">
+      <div className="flex items-center flex-wrap">
+        {tags?.map((tag, idx) => (
+          <span
+            key={idx}
+            className="bg-gray-400 text-white px-2 rounded-full flex items-center my-0.5 text-sm mr-2"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+
+      <label className="sr-only">{label}</label>
+      <input
+        value={inputValue}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setInputValue(e.target.value)
+        }
+        onCompositionStart={() => setIsComposing(true)}
+        onCompositionEnd={() => setIsComposing(false)}
+        onKeyDown={handleTagEnter}
+        className="relative item-center pb-1.5 text-sm placeholder-gray-400 text-gray-400 flex-1"
+        type="text"
+        name="tags"
+        placeholder={
+          tags?.length === 0
+            ? '쉼터 혹은 엔터를 입력하여 태그를 등록할 수 있습니다.'
+            : ''
+        }
+      />
+    </div>
+  );
+}
+
+export default TagInput;
