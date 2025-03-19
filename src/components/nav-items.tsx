d26b2f5ac85d1de4bd83474431f9348fa8220f7e
akman12914 <rnlduadns98@naver.com>
2025-03-20 02:24:26 +0900

    [Feat] my/피드작성 진입 금지

--- src/components/nav-items.tsx
+++ src/components/nav-items.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { toast } from 'react-toastify'; // react-toastify import
+
 interface NavItem {
   id: string;
   src: string;
@@ -35,8 +37,17 @@ const navItems: NavItem[] = [
 function NavItems() {
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
 
+  // 로컬 스토리지에서 isAuth 키 값을 확인
+  const isAuthenticated = localStorage.getItem('isAuth');
+
   const handleClick = (id: string) => {
     setSelectedItem(id);
+
+    // 피드작성(plus)와 MY(my)만 인증 여부 체크
+    if ((id === 'plus' || id === 'my') && !isAuthenticated) {
+      toast.error('로그인이 필요합니다!'); // 경고 메시지 표시
+      return; // 링크로 진입하지 않음
+    }
   };
 
   return (
@@ -45,10 +56,17 @@ function NavItems() {
         {navItems.map((item) => (
           <li
             key={item.id}
-            onClick={() => handleClick(item.id)}
+            onClick={() => handleClick(item.id)} // 클릭 시 로직 추가
             className={` ${selectedItem === item.id ? 'shadow-[0_-2px_0_0_black]' : ''} `}
           >
-            <Link href={item.href}>
+            <Link
+              href={
+                // 인증되지 않으면 '피드작성'과 'MY' 메뉴는 #으로 막고, 나머지는 정상 동작
+                (item.id === 'plus' || item.id === 'my') && !isAuthenticated
+                  ? '#'
+                  : item.href
+              }
+            >
               <figure className="flex flex-col items-center px-2 py-2">
                 <Image src={item.src} width={24} height={24} alt={item.alt} />
                 <figcaption>{item.label}</figcaption>
