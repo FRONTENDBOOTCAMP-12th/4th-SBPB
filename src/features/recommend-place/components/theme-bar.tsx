8532c250290fca678db11087eb5df36a749f11ae
llhyeon <0617mosses@gmail.com>
2025-03-19 14:09:56 +0900

    [Design] 스타일링 변경
    Resolves #141

--- src/features/recommend-place/components/theme-bar.tsx
+++ src/features/recommend-place/components/theme-bar.tsx
@@ -2,13 +2,13 @@ function ThemeBar() {
   return (
     <article className="flex justify-center gap-1">
       <button
-        className="font-semibold border-b-3 border-transparent text-xs py-1 my-2 w-22 focus:border-b-3 focus:border-accent focus:text-accent"
+        className="font-semibold border-b-3 border-content-primary text-xs py-1 my-2 w-22 focus:border-b-3 focus:border-accent focus:text-accent"
         type="button"
       >
         전국 여행지
       </button>
       <button
-        className="font-semibold border-b-3 border-transparent text-xs py-1 my-2 w-22 focus:border-b-3 focus:border-accent focus:text-accent"
+        className="font-semibold border-b-3 border-content-primary text-xs py-1 my-2 w-22 focus:border-b-3 focus:border-accent focus:text-accent"
         type="button"
       >
         관심 지역
