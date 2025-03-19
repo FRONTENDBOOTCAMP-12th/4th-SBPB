8f490f7fb11b288cf28c8bea7a7b3f3f2b8e3c18
llhyeon <0617mosses@gmail.com>
2025-03-19 14:09:29 +0900

    [Feat] 서버 데이터 받아와서 props 전달
    - supabase 에서 태그 데이터 받아와서 렌더링
    
    Resolves #141

--- src/app/recommend-place/page.tsx
+++ src/app/recommend-place/page.tsx
@@ -1,10 +1,31 @@
 import NavItems from '@/components/nav-items';
+import TagFilter from '@/features/recommend-place/components/tag-filter';
 import ThemeBar from '@/features/recommend-place/components/theme-bar';
+import { createClient } from '@/utils/supabase/server';
 
-function RecommendPlacePage() {
+async function RecommendPlacePage() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from('post')
+    .select('tags')
+    .range(0, 49);
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const tagArr = data.flatMap((tag) => {
+    return tag.tags.split(',');
+  });
+
+  const tags = Array.from(new Set(tagArr));
+
   return (
     <section>
       <ThemeBar />
+      <TagFilter tags={tags} />
       <NavItems />
     </section>
   );
