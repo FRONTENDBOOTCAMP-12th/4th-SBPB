a381e3243a024005e9776956225dcb169b6f54ff
neulhi <neu1hi.work@gmail.com>
2025-03-20 10:54:12 +0900

    [Design] app/layout 스크롤 관련 속성 추가
    - 스크롤바 사라졌다 없어지면서 레이아웃 흔들려서 app/layout에 'overflow-y-scroll' 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className="overflow-y-scroll">
       <body>
         <Toast />
         {children}
