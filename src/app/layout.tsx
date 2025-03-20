a5e12940f5f7b0b39b13ed31c8070842b8a69431
neulhi <neu1hi.work@gmail.com>
2025-03-20 16:51:36 +0900

    [Design] 루트 레이아웃에 최대 크기 320px 테스트
    Resolves #155

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className="overflow-y-scroll">
+    <html lang="ko" className="overflow-y-scroll max-w-[320px] mx-auto">
       <body>
         <Toast />
         {children}
