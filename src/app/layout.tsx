85f3feb56298517da3ac5cbe0c607b5133b100cc
akman12914 <rnlduadns98@naver.com>
2025-03-22 17:04:37 +0900

    [Refactor] 스크립트 일부 페이지 레이아웃으로 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,6 @@ export const metadata: Metadata = {
   },
 };
 
-const KAKAO_API_KEY_ID: string | undefined = process.env.KAKAO_API_KEY_ID;
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -41,10 +39,6 @@ export default function RootLayout({
       <body>
         <Toast />
         {children}
-        <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY_ID}&libraries=services&autoload=false`}
-          strategy="beforeInteractive"
-        />
       </body>
     </html>
   );
